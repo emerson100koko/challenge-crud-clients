354ef50e20d4dd715eb48394fce98552918236e8
emerson <emerson100loko@hotmail.com>
2023-04-20 23:41:15 -0300

    correcao seed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,8 +3,8 @@ INSERT INTO tb_client (id, name, cpf, income, bith_date, children) VALUES (2, 'C
 INSERT INTO tb_client (id, name, cpf, income, bith_date, children) VALUES (3, 'Aline Aguiar', '23073360000', 3100.0, '2023-10-29', 1);
 INSERT INTO tb_client (id, name, cpf, income, bith_date, children) VALUES (4, 'Emerson Gomes', '49079860840', 4000.0, '2023-10-19', 1);
 INSERT INTO tb_client (id, name, cpf, income, bith_date, children) VALUES (5, 'Marcelo Moreno', '80073360111', 4000.0, '2023-07-23', 0);
-INSERT INTO tb_client (id, name, cpf, income, bith_date, children) VALUES (6, 'Kleberson Alonso', '90903360850', 6000.0, '2023-12-19', 3);
-INSERT INTO tb_client (id, name, cpf, income, bith_date, children) VALUES (7, 'Amanda Nunes', '990788808786', 3500.0, '2023-03-03', 1);
+INSERT INTO tb_client (id, name, cpf, income, bith_date, children) VALUES (6, 'Kleberson Alonso', '90903360050', 6000.0, '2023-12-19', 3);
+INSERT INTO tb_client (id, name, cpf, income, bith_date, children) VALUES (7, 'Amanda Nunes', '99078880878', 3500.0, '2023-03-03', 1);
 INSERT INTO tb_client (id, name, cpf, income, bith_date, children) VALUES (8, 'Silvana Maria', '10012060850', 2600.0, '2023-02-09', 0);
 INSERT INTO tb_client (id, name, cpf, income, bith_date, children) VALUES (9, 'Angel Romero', '99093360191', 5050.0, '2023-04-02', 4);
 INSERT INTO tb_client (id, name, cpf, income, bith_date, children) VALUES (10, 'Eder Araujo', '55573360000', 3900.0, '2023-06-30', 0);
